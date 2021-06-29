af245babe1eb712b7384e70ead5ed9f9016b3111
Ho Nguyen Tri Nhan <nhan0385790927@gmail.com>
2021-06-29 14:12:05 +0700

    initializing customer site project

---
+++ EcommerceSite/EcommerceSite/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using EcommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
